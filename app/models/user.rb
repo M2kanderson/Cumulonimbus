1c70afebad7aa040d3a9d57be017388f433fdea1
Justin Anderson <janderson2k@gmail.com>
2016-07-18 11:48:18 -0700

    added facebook omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  # attr_accessible :email, :name, :password, :password_confirmation, :remember_me
+
+  has_many :authorizations, :dependent => :destroy
 end
