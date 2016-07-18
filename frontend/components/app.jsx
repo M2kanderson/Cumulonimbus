0ba3f97a6daad99f26f7757dc8e1dd19ffb266e6
Justin Anderson <janderson2k@gmail.com>
2016-07-18 16:24:25 -0700

    Hello World initialized

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    console.log("test");
+    return (
+      <div>Hello World!</div>
+    );
+  }
+
+});
+
+module.exports = App;
