63a1ee61adebe420146b7def1afdff3fe3f29ae6
Mark Mullan <markrmullan@gmail.com>
2016-07-19 12:47:34 -0700

    login modal

--- frontend/components/login_form.jsx
+++ frontend/components/login_form.jsx
@@ -58,6 +58,7 @@ const LoginForm = React.createClass({
       password: this.state.password
     };
     SessionActions.login(userData);
+    hashHistory.push('/');
   },
 
   openModal: function() {
